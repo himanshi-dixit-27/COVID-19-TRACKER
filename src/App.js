f4ba6f2f38a6310b265674278f773ecba818f4bc
himanshi-dixit-27 <hd271198@gmail.com>
2020-11-01 01:54:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app"> //BEM Naming convention
+    <div className="app">
       <h1>Let's Build a COVID-19 tracker</h1>
     </div>
   );
